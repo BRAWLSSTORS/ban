978318529e5e469e8d797e341a41260a0d88829b
BRAWLSSTORS <brawlpkt@gmail.com>
2024-10-29 13:39:18 +0200

    Update main.py

--- main.py
+++ main.py
@@ -8,7 +8,7 @@ import logging
 import time
 import random
 
-# Настройка логированияz
+# Настройка логирования
 logging.basicConfig(level=logging.INFO)
 logger = logging.getLogger(__name__)
 
@@ -17,7 +17,7 @@ API_TOKEN = '7368730334:AAH9xUG8G_Ro8mvV_fDQxd5ddkwjxHnBoeg'
 
 bot = telebot.TeleBot(API_TOKEN)
 
-def take_screenshot(url):
+def take_screenshot_and_get_email(url):
     chrome_options = Options()
     chrome_options.add_argument("--headless")  # Открытие браузера в фоновом режиме
     chrome_options.add_argument("--no-sandbox")
@@ -31,17 +31,23 @@ def take_screenshot(url):
     try:
         driver.get(url)
         logger.info(f"Загружена страница: {url}")
-        time.sleep(random.uniform(2, 5))  # Случайная задержка после загрузки страницы
+        time.sleep(random.uniform(2, 5))  # Случайная задержка
+
+        # Извлечение текста из атрибута title
+        email_element = driver.find_element('css selector', 'input#email[email_input]')
+        email_title = email_element.get_attribute('title')
+        logger.info(f"Извлеченный email: {email_title}")
 
         # Скриншот страницы
         screenshot_path = 'tempmail_screenshot.png'
         driver.save_screenshot(screenshot_path)
         logger.info("Скриншот сохранен")
-        return screenshot_path
+
+        return screenshot_path, email_title
 
     except Exception as e:
         logger.error(f"Произошла ошибка: {str(e)}")
-        return None
+        return None, None
 
     finally:
         driver.quit()
@@ -49,13 +55,15 @@ def take_screenshot(url):
 @bot.message_handler(commands=['tempmail'])
 def handle_tempmail(message):
     url = "https://temp-mail.io/ru"
-    screenshot_path = take_screenshot(url)
+    screenshot_path, email_title = take_screenshot_and_get_email(url)
 
-    if screenshot_path:
+    if screenshot_path and email_title:
         with open(screenshot_path, 'rb') as file:
             bot.send_photo(message.chat.id, file)
         os.remove(screenshot_path)
+
+        bot.send_message(message.chat.id, f"Ваш временный email: {email_title}")
     else:
-        bot.reply_to(message, "Произошла ошибка при создании скриншота. Попробуйте снова.")
+        bot.reply_to(message, "Произошла ошибка при создании скриншота или получении email. Попробуйте снова.")
 
 bot.polling()
